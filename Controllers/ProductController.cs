6e4f6054b297902715d536cfae8692620710019c
Mauricio Roberto Gonzatto <mauriciogonzatto@hotmail.com>
2020-03-31 13:45:30 -0300

    SportsStore

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgWeb202.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgWeb202.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        // O parâmetro definido como Injeção de Dependência
+        // e está previamente registrado como serviço do 
+        // arquivo Startup.cs
+        public ProductController( IProductRepository repo ) {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+        
+    }
+}
---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProgWeb202.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+            new Product{ Name = "Bola de Futebol", Price = 25 },
+            new Product{ Name = "Bola de Ping-Pong", Price = 5 }
+        }.AsQueryable<Product>();
+    }
+}
